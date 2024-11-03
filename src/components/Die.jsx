1dd6bc9536c2ed8caa6c287ca2978746abfe5106
JoleneKearse <pharaohnutz@gmail.com>
2024-11-03 15:50:07 -0500

    chore: update handleClick

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,11 +1,14 @@
 const Die = ({ value, isHeld, id, holdDie }) => {
-  const handleClick = (e) => {
-    holdDie(e.target.id);
+  const handleClick = () => {
+    holdDie(id);
   };
 
   return (
-    <button className={`rounded-lg w-11 h-11 shadow-xl text-xl font-semibold border-b-4 border-r-2 lg:w-16 lg:h-16 ${isHeld ? "bg-accent border-brand-500" : "bg-brand-200 border-slate-200"}`} id={id}
-    onClick={handleClick}>
+    <button
+      className={`rounded-lg w-11 h-11 shadow-xl text-xl font-semibold border-b-4 border-r-2 lg:w-16 lg:h-16 ${isHeld ? "bg-accent border-brand-500" : "bg-brand-200 border-slate-200"}`}
+      id={id}
+      onClick={handleClick}
+    >
       {value}
     </button>
   )
