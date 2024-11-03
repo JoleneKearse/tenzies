2962e409988983a8e582115a0bc157b4c74e5f0f
JoleneKearse <pharaohnutz@gmail.com>
2024-11-03 15:10:37 -0500

    chore: fix prettier to reformat

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import { nanoid } from "nanoid";
 import DieGrid from "./components/DieGrid";
 import Button from "./components/Button";
 
-
 function App() {
   const [dice, setDice] = useState(startingDice());
   const [tenzies, setTenzies] = useState(false);
-  const [buttonText, setButtonText] = useState("Roll")
+  const [buttonText, setButtonText] = useState("Roll");
 
   useEffect(() => {
     const firstValue = dice[0].value;
     if (dice.every((die) => die.value === firstValue && die.isHeld)) {
       setTenzies(true);
-      setButtonText("New Game")
+      setButtonText("New Game");
     }
   }, [dice]);
 
@@ -25,7 +24,7 @@ function App() {
 
   function getRandomDieValue() {
     return Math.ceil(Math.random() * 6);
-  };
+  }
 
   function startingDice() {
     return Array.from({ length: 10 }, () => ({
@@ -33,15 +32,17 @@ function App() {
       isHeld: false,
       id: nanoid(),
     }));
-  };
+  }
 
   const handleClick = () => {
     if (tenzies) {
       restartGame();
-      return;
-    };
-    setDice((prevDice) => prevDice.map((die) =>
-      die.isHeld ? die : { ...die, value: getRandomDieValue() }))
+    }
+    setDice((prevDice) =>
+      prevDice.map((die) =>
+        die.isHeld ? die : { ...die, value: getRandomDieValue() }
+      )
+    );
   };
 
   const holdDie = (id) => {
@@ -55,11 +56,21 @@ function App() {
   return (
     <main className="w-72 h-4/5 bg-brand-100 rounded-3xl my-32 mx-auto p-10 flex flex-col justify-center items-center gap-5 md:w-1/2 md:gap-8 lg:w-2/3 lg:gap-12">
       <h1 className="text-4xl font-black md:text-5xl lg:text-7xl">Tenzies</h1>
-      <p className="max-w-prose text-balance text-center text-slate-700">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-      <DieGrid dice={dice} holdDie={holdDie} tenzies={tenzies} />
-      <Button onClick={handleClick} buttonText={buttonText} />
+      <p className="max-w-prose text-balance text-center text-slate-700">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <DieGrid
+        dice={dice}
+        holdDie={holdDie}
+        tenzies={tenzies}
+      />
+      <Button
+        onClick={handleClick}
+        buttonText={buttonText}
+      />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/DieGrid.jsx
+++ src/components/DieGrid.jsx
@@ -5,9 +5,34 @@ const DieGrid = ({ dice, holdDie, tenzies }) => {
   return (
     <article className="grid grid-cols-5 grid-rows-[auto_auto] gap-10 mx-auto mb-6 justify-items-center items-center">
       {dice.map((die) => (
-        <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDie={holdDie} id={die.id} />
+        <Die
+          key={die.id}
+          value={die.value}
+          isHeld={die.isHeld}
+          holdDie={holdDie}
+          id={die.id}
+        />
       ))}
-      {tenzies && <ConfettiExplosion force={.8} duration={3000} particleCount={250} height={'300vh'} width={1600} colors={["#009ff6", "#0a2433", "#005fb6", "#f9f871", "#90d577", "#007c79", "#e99b26", "#6eebb4"]} />}
+      {tenzies &&
+        <ConfettiExplosion
+          force={.8}
+          duration={3000}
+          particleCount={250}
+          height={'300vh'}
+          width={1600}
+          colors={
+            [
+              "#009ff6",
+              "#0a2433",
+              "#005fb6",
+              "#f9f871",
+              "#90d577",
+              "#007c79",
+              "#e99b26",
+              "#6eebb4"
+
+            ]}
+        />}
     </article>
   )
 }
