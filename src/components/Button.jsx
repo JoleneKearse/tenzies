451b7da3fd26bb7cdbd01d7fa1d4eb24c1eb92e3
JoleneKearse <pharaohnutz@gmail.com>
2024-11-03 12:17:01 -0500

    chore: adjust dice styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ onClick }) => {
 
   return (
-    <button className="bg-brand-500 py-2 px-8 rounded-3xl text-brand-200 font-bold text-2xl hover:bg-brand-700 md:text-3xl lg:text-4xl" onClick={onClick}>
+    <button className="bg-brand-600 py-2 px-8 rounded-3xl text-brand-200 font-bold text-2xl hover:bg-brand-700 md:text-3xl lg:text-4xl" onClick={onClick}>
       Roll
     </button>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ export default {
         "brand-100": "#f5f5f5",
         "brand-200": "#ffffff",  // dice
         "brand-400": "#6eebb4",  // selected dice
-        "brand-500": "#009ff6",  // button
+        "brand-500": "#42BF88",  // selected dice
+        "brand-600": "#009ff6",  // button
         "brand-700": "#005fb6",  // button hover/focus
         "brand-900": "#0a2433",
         accent: "#00c6b1"
